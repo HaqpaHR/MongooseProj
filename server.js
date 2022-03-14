6765bf013583f567970d71af036c38f2747983ca
HaqpaHR <pereverzev.viacheslav@gmail.com>
2022-03-14 16:16:13 +0200

    fix some bugs, add list of users

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config().parsed;
+}
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -14,12 +15,13 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
-mongoose.createConnection(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open ", () => console.log("Connected to mongoose"));
+db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
